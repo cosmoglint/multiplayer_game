892bb41c453bf7ac00518de025add9c005a03942
thedoodler <ramprakashsmail@gmail.com>
2020-10-01 14:44:38 +0530

    working version

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,19 @@
 
+var socket;
+
+
 function setup(){
   createCanvas(200,200);
   background(20);
+  socket = io.connect('http://localhost:3000');
+}
+
+function mouseDragged(){
+  console.log(mouseX + "," + mouseY);
+  strokeWeight(0);
+  fill('white');
+  circle(mouseX,mouseY,20);
 }
 
 function draw(){
-  fill('white');
-  circle(mouseX,mouseY,60);
 }
--- server.js
+++ server.js
@@ -7,3 +7,14 @@ var server = app.listen(3000);
 app.use(express.static('public'));
 
 console.log("the server is running");
+
+
+var socket = require('socket.io');
+
+var io = socket.listen(server);
+
+io.sockets.on('connection', new_connection);
+
+function new_connection(socket){
+  console.log("new connection" + socket.id );
+}
